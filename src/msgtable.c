cba60ec4186f61a5ee4f4c7f11efb9f50ff8a0b5
Michał Siejak <michal@siejak.pl>
2011-09-13 17:25:52 +0200

    Updated protocol message table to be 1.8-pre2 compatible.

--- src/msgtable.c
+++ src/msgtable.c
@@ -16,9 +16,9 @@
 #define PROTOCOL_VERSION 15
 #define _defmsg(id, format) { id, format, NULL, NULL, NULL, NULL }
 
-msgdesc_t msgtable[] = {
+const msgdesc_t msgtable[] = {
   _defmsg(0x00, "i"), // Keep Alive
-  _defmsg(0x01, "itliccc"), // Login Request
+  _defmsg(0x01, "itlicccc"), // Login Request
   _defmsg(0x02, "t"), // Handshake
   _defmsg(0x03, "t"), // Chat message
   _defmsg(0x04, "l"), // Time update
@@ -26,7 +26,7 @@ msgdesc_t msgtable[] = {
   _defmsg(0x06, "iii"), // Spawn position
   _defmsg(0x07, "iic"), // Use entity
   _defmsg(0x08, "ssf"), // Update health
-  _defmsg(0x09, "ccsl"), // Respawn
+  _defmsg(0x09, "cccsl"), // Respawn
   _defmsg(0x0A, "c"), // Player
   _defmsg(0x0B, "ddddc"), // Player position
   _defmsg(0x0C, "ffc"), // Player look
@@ -34,7 +34,7 @@ msgdesc_t msgtable[] = {
   _defmsg(0x0E, "cicic"), // Player digging
   _defmsg(0x0F, "icics--"), // Player block placement (datahelper)
   _defmsg(0x10, "s"), // Holding change
-  _defmsg(0x11, "icici"), // Unknown (NEW IN 1.3)
+  _defmsg(0x11, "icici"), // Use bed
   _defmsg(0x12, "ic"), // Animation
   _defmsg(0x13, "ic"), // Entity action
   _defmsg(0x14, "itiiiccs"), // Named entity spawn
@@ -43,7 +43,8 @@ msgdesc_t msgtable[] = {
   _defmsg(0x17, "iciiii---"), // Add object/vehicle (datahelper)
   _defmsg(0x18, "iciiiccm"), // Mob spawn
   _defmsg(0x19, "itiiii"), // Painting
-  _defmsg(0x1B, "ffffcc"), // Unknown (NEW IN 1.3)
+  _defmsg(0x1A, "iiiis"), // Unknown
+  _defmsg(0x1B, "ffffcc"), // Stance update
   _defmsg(0x1C, "isss"), // Entity velocity
   _defmsg(0x1D, "i"), // Destroy entity
   _defmsg(0x1E, "i"), // Entity
@@ -63,10 +64,10 @@ msgdesc_t msgtable[] = {
   _defmsg(0x35, "icicc"), // Block change
   _defmsg(0x36, "isicc"), // Play note block
   _defmsg(0x3C, "dddfi"), // Explosion (datahelper)
-  _defmsg(0x3D, "iicii"), // Door change (new in 1.6)
+  _defmsg(0x3D, "iicii"), // Door change
   _defmsg(0x46, "cc"), // New/Invalid state
   _defmsg(0x47, "iciii"), // Weather
-  _defmsg(0x64, "ccuc"), // Open window
+  _defmsg(0x64, "cctc"), // Open window
   _defmsg(0x65, "c"), // Close window
   _defmsg(0x66, "cscscs--"), // Window click (datahelper)
   _defmsg(0x67, "css--"), // Set slot (datahelper)
@@ -75,7 +76,7 @@ msgdesc_t msgtable[] = {
   _defmsg(0x6A, "csc"), // Transaction
   _defmsg(0x6B, "ssss"), // Creative inventory action
   _defmsg(0x82, "isitttt"), // Update sign
-  _defmsg(0x83, "ssc"), // Map data (new in 1.6) (datahelper)
+  _defmsg(0x83, "ssc"), // Map data (datahelper)
   _defmsg(0xC8, "ic"), // Increment statistic
   _defmsg(0xC9, "tcs"), // User list item
   _defmsg(0xFE, NULL), // Server list ping
