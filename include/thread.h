8f00d36f7be57dd0871064c80b25228822a1fd2b
Michał Siejak <michal@siejak.pl>
2011-09-24 23:35:01 +0200

    Expanded thread subsystem by some easy spawn/join helper functions and generic sync barrier.

--- include/thread.h
+++ include/thread.h
@@ -11,7 +11,14 @@
 #include <stdint.h>
 #include <pthread.h>
 
+#define THREAD_DETACHED 0x01
+#define THREAD_NOPOOL   0x02
+
 #define MCP_TLS_INITIALIZER { 0, 0 }
+
+// Opaque thread object type
+typedef struct thread_object_t* thread_object_t;
+typedef int (*thread_func_t)(void*);
 
 struct tls_value_s
 {
@@ -27,5 +34,12 @@ void  thread_tls_set(tls_value_t* value, void* ptr);
 void  thread_lock(void);
 void  thread_unlock(void);
 int   thread_trylock(void);
+
+thread_object_t thread_create(thread_func_t entrypoint, void* data,
+			      unsigned long flags, size_t pool_size);
+thread_object_t thread_self(void);
+
+int  thread_join(thread_object_t thread);
+void thread_sync(unsigned short count);
 
 #endif
--- src/thread.c
+++ src/thread.c
@@ -6,9 +6,51 @@
  */
 
 #include <stdlib.h>
-#include <thread.h>
 
-static pthread_mutex_t default_mutex = PTHREAD_MUTEX_INITIALIZER;
+#include <config.h>
+#include <thread.h>
+#include <system.h>
+#include <mm.h>
+
+static pthread_mutex_t   default_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t   barrier_mutex = PTHREAD_MUTEX_INITIALIZER;
+static unsigned short    barrier_count = 0;
+static pthread_barrier_t default_barrier;
+
+struct entrypoint_info_s
+{
+  thread_func_t user_func;
+  void* user_data;
+  unsigned long flags;
+  size_t pool_size;
+};
+typedef struct entrypoint_info_s entrypoint_info_t;
+
+static void* _thread_entrypoint(void* data)
+{
+  entrypoint_info_t* entrypoint_info = (entrypoint_info_t*)data;
+  mempool_t thread_pool;
+  int retvalue;
+
+  if(entrypoint_info->flags & THREAD_DETACHED)
+    pthread_detach(pthread_self());
+
+  if(!(entrypoint_info->flags & THREAD_NOPOOL)) {
+    size_t pool_size = entrypoint_info->pool_size;
+    if(pool_size == 0)
+      pool_size = sys_get_config()->pool_size;
+    if(pool_create(&thread_pool, pool_size))
+      pool_set_default(&thread_pool);
+  }
+
+  retvalue = entrypoint_info->user_func(entrypoint_info->user_data);
+
+  if(!(entrypoint_info->flags & THREAD_NOPOOL))
+    pool_release(&thread_pool);
+
+  free(entrypoint_info);
+  pthread_exit((void*)(size_t)retvalue);
+}
 
 void thread_tls_initonce(tls_value_t* value)
 {
@@ -41,3 +83,51 @@ int thread_trylock(void)
   return pthread_mutex_trylock(&default_mutex);
 }
 
+thread_object_t thread_create(thread_func_t entrypoint, void* data,
+			      unsigned long flags, size_t pool_size)
+{
+  pthread_t thread_id;
+  entrypoint_info_t* info = malloc(sizeof(entrypoint_info_t));
+  info->user_func = entrypoint;
+  info->user_data = data;
+  info->flags     = flags;
+  info->pool_size = pool_size;
+
+  if(pthread_create(&thread_id, NULL, _thread_entrypoint, (void*)info) != 0) {
+    free(info);
+    return NULL;
+  }
+  return (thread_object_t)thread_id;
+}
+
+int thread_join(thread_object_t thread)
+{
+  void* return_data;
+  if(pthread_join((pthread_t)thread, &return_data) != 0)
+    return -1;
+  return (int)(size_t)return_data;
+}
+
+void thread_sync(unsigned short count)
+{
+  pthread_mutex_lock(&barrier_mutex);
+  if(barrier_count != count) {
+    pthread_barrier_init(&default_barrier, NULL, count);
+    barrier_count = count;
+  }
+  pthread_mutex_unlock(&barrier_mutex);
+
+  pthread_barrier_wait(&default_barrier);
+
+  pthread_mutex_lock(&barrier_mutex);
+  if(barrier_count > 0) {
+    pthread_barrier_destroy(&default_barrier);
+    barrier_count = 0;
+  }
+  pthread_mutex_unlock(&barrier_mutex);
+}
+
+thread_object_t thread_self(void)
+{
+  return (thread_object_t)pthread_self();
+}
