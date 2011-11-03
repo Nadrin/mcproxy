9a15d958b324e54f1895af6213771256059f2c1e
Dylan Lukes <lukes.dylan@gmail.com>
2011-11-03 13:46:42 -0400

    fixed not checking endianness. naive and could use optimziation.

--- src/endian.c
+++ src/endian.c
@@ -53,35 +53,41 @@ inline int64_t mcp_swapq(int64_t x)
 
 inline int16_t mcp_htons(int16_t host_short)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) host_short = mcp_swaps(host_short);
 	return host_short;
 }
 
 inline int32_t mcp_htonl(int32_t host_long)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) host_long = mcp_swapl(host_long);
 	return host_long;
 }
 
 inline int64_t mcp_htonq(int64_t host_quad)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) host_quad = mcp_swapq(host_quad);
 	return host_quad;
 }
 
 inline int16_t mcp_ntohs(int16_t net_short)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) net_short = mcp_swaps(net_short);
 	return net_short;
 }
 inline int32_t mcp_ntohl(int32_t net_long)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) net_long = mcp_swapl(net_long);
 	return net_long;
 }
 
 inline int64_t mcp_ntohq(int64_t net_quad)
 {
+  mcp_test_byte_order();
 	if(mcp_byte_order == MCP_LITTLE_ENDIAN) net_quad = mcp_swapq(net_quad);
 	return net_quad;
 }
